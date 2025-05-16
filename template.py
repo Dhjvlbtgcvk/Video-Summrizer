4d265675edc6e7337e8b1f43de097ac38d492881
shahzaman <gallanizaman@gmail.com>
2025-05-16 12:21:21 -0700

    file structure is created

---
+++ template.py
@@ -0,0 +1,26 @@
+﻿from pathlib import Path
+import os
+
+list_of_files = [
+    
+    "app.py",
+    "reseach/test.py",
+    "reseach/test.ipynb",
+    ".env",
+    "requirements.txt"
+    
+]
+
+for files in list_of_files:
+    files_path = Path(files)
+    
+    dir_name,file_name = os.path.split(files_path)
+    print(dir_name,file_name)
+    
+    if dir_name:
+        os.makedirs(dir_name,exist_ok=True)
+    if not os.path.exists(files_path):
+        with open(files_path,'w') as f:
+            pass
+    else:
+        print("not file existsS")
